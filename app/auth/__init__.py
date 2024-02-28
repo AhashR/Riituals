010d21d06e7f8de1a3dd488aad0fa50cb7f1ca35
Jesper Jonk <jesper.jonk@hva.nl>
2024-02-28 15:50:47 +0100

    Login systeem

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -9,32 +9,37 @@ from flask import session, redirect, url_for
 
 bp = Blueprint('auth', __name__)
 
-def add_user(firstname, lastname, email, phone, password):
-    query = "INSERT INTO User (firstname, lastname, emailaddress, telephonenumber, password) VALUES (%s, %s, %s, %s, %s)"
-    values = (firstname, lastname, email, phone, password)
+def add_user(name, location, branchnumber, email, phone, password):
+    query = "INSERT INTO User (name, location, branchnumber, emailaddress, telephonenumber, password, isHandler) VALUES (%s, %s, %s, %s, %s, %s, 0)"
+    values = (name, location, branchnumber, email, phone, password)
     execute_query(query, values)
 
+def add_admin(name, location, branchnumber, email, phone, password):
+    query = "INSERT INTO User (name, location, branchnumber, emailaddress, telephonenumber, password, isHandler) VALUES (%s, %s, %s, %s, %s, %s, 1)"
+    values = (name, location, branchnumber, email, phone, password)
+    execute_query(query, values)
     
 # gebruiker gegevens aanpassen zoals naam en wachtwoord gegevens #
-def update_user(id, firstname, lastname, email, phone, password):
-    query = "UPDATE User SET firstname = %s, lastname = %s, emailaddress = %s, \
-        telephonenumber = %s, password = %s WHERE userId = %s"
-    values = (firstname, lastname, email, phone, password, id)
+def update_user(name, location, branchnumber, email, phone, password, userId):
+    query = "UPDATE User SET name = %s, location = %s, branchnumber = %s, emailaddress = %s, telephonenumber = %s, password = %s WHERE userId = %s"
+    values = (name, location, branchnumber, email, phone, password, userId)
     execute_query(query, values)
+
 
 @bp.before_app_request
 def load_logged_in_user():
     user_id = session.get('user_id')
-    if user_id is not None:
-        g.user = select_one("SELECT * FROM User WHERE userId = %s", user_id)
-    else:
+    if user_id is None:
         g.user = None
+    else:
+        g.user = select_one("SELECT * FROM User WHERE userId = %s", user_id)
+        g.isHandler = g.user['isHandler']
 # je moet ingelogd zijn om bepaalde functie te gebruiken #
 def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
-            return redirect(url_for('auth.login'))
+            return redirect(url_for('main.index'))
         return view(**kwargs)
     return wrapped_view
 
--- db/schema.sql
+++ db/schema.sql
@@ -10,32 +10,16 @@ USE `Rituals`;
 CREATE TABLE `User`(
     -- Minimal user table, adjust up to your needs
 	`userId` INT NOT NULL AUTO_INCREMENT,	
-    `firstname` VARCHAR(50),
-    `lastname` VARCHAR(100),
-	`emailaddress` VARCHAR(250),
+    `name` VARCHAR(50) NOT NULL,
+    `location` VARCHAR(100),
+    `branchnumber` VARCHAR(50),
+	`emailaddress` VARCHAR(250) NOT NULL,
     `telephonenumber` VARCHAR(15),
-    `password` VARCHAR(15),
+    `password` VARCHAR(15) NOT NULL,
+    `isHandler` BOOLEAN NOT NULL,
     PRIMARY KEY(`userId`)
 );
 
-CREATE TABLE `handlers` (
-    `handlerId` INT NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(255) NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255),
-    `phoneNumber` VARCHAR(20),
-    PRIMARY KEY(`handlerId`)
-);
-
--- Employees table
-CREATE TABLE `employees` (
-    `employeeId` INT NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(255) NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255),
-    `phoneNumber` VARCHAR(20),
-    PRIMARY KEY(`employeeId`)
-);
 
 -- Add your create tables SQL here. Make sure to include the indexes!
 -- Alternative: use an ORM like SQLAlchemy. Then you will not need this file.
