41a08f21478ad78c63147b760e3a7355fa437553
Jesper Jonk <jesper.jonk@hva.nl>
2024-03-21 10:51:54 +0100

    Inplannen via agenda

--- app/handler/__init__.py
+++ app/handler/__init__.py
@@ -4,5 +4,18 @@ from datetime import datetime
 
 bp = Blueprint('handler', __name__)
 
+def check_date(arrivalDate):
+    result = select_one("SELECT dateId FROM Deliverydate WHERE arrivalDate = %s", arrivalDate)
+    if result is None:
+        query = "INSERT INTO Deliverydate (arrivalDate) VALUES (%s)"
+        values = (arrivalDate)
+        execute_query(query, values)
+        result = select_one("SELECT dateId FROM Deliverydate WHERE arrivalDate = %s", arrivalDate)
+    return result['dateId']
+
+def userBranchnumber(branchnumber):
+    result = select_one("SELECT userId FROM User WHERE branchnumber = %s", branchnumber)
+    return result['userId']
+
 
 from app.handler import routes
